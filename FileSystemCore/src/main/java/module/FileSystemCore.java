27799174b4038e875b113819f9816a4e58ea2512
nikola <ngavrilovic7822rn@raf.rs>
2022-10-16 17:31:30 +0200

    Cekiranje i kreiranje file path-a

--- FileSystemCore/src/main/java/module/FileSystemCore.java
+++ FileSystemCore/src/main/java/module/FileSystemCore.java
@@ -1,5 +1,5 @@
 package module;
 
 public interface FileSystemCore {
-    void initialize();
+
 }
--- FileSystemCore/src/main/java/module/FileSystemCoreImplementation.java
+++ FileSystemCore/src/main/java/module/FileSystemCoreImplementation.java
@@ -1,5 +1,6 @@
 package module;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -13,14 +14,18 @@ public class FileSystemCoreImplementation implements FileSystemCore{
 
         this.storagePath = Paths.get(desktopPath + "/" + storageFolderName); // Default path je na desktop-u
         System.out.println(storagePath.toString());
+        initialize();
     }
 
     public FileSystemCoreImplementation(String path){
         this.storagePath = Paths.get(path);
+        initialize();
     }
 
-    @Override
-    public void initialize() {
-
+    private void initialize() {
+        File storageFile = storagePath.toFile();
+        if(!storageFile.exists()) {
+            storageFile.mkdir();
+        }
     }
 }
