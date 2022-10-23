92d318331314d3ee71674ef0679dbbc92afcbb6e
MimiKing01 <milanj1705@gmail.com>
2022-10-23 02:23:57 +0200

    Test implementation of the module

--- FileSystemCore/src/main/java/main/Main.java
+++ FileSystemCore/src/main/java/main/Main.java
@@ -3,8 +3,21 @@ package main;
 import module.FileSystemCore;
 import module.FileSystemCoreImplementation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-        FileSystemCore fileSystemCore = new FileSystemCoreImplementation();
+        List<String> ext = new ArrayList<>();
+        FileSystemCore fileSystemCore = new FileSystemCoreImplementation(100000, ext, 55);
+
+        //fileSystemCore.uploadFile("D:\\RuDok.zip","/");
+        //fileSystemCore.deleteFile("/RuDok.zip");
+        //fileSystemCore.moveFile("/RuDok.zip", "/test");
+        //fileSystemCore.renameFile("/RuDok.zip", "RuDok2.zip");
+        //fileSystemCore.createFile("test.txt", "/");
+        //fileSystemCore.downloadFile("/RuDok2.zip", "C:\\Users\\Milan\\Desktop");
+
+
     }
 }
